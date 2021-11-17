560022867a6d83b1a80288d7f0ffe526472ce82c
TanyaHendricks <bob4robert@gmail.com>
2021-11-18 01:05:40 +0200

    Built Photos Page

--- photo_album/photos/views.py
+++ photo_album/photos/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .models import Album, Image, Metadata
+
+import datetime
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    albums = Album.objects.all() 
+    images = Image.objects.all() 
+    context = {
+        'albums': albums,
+        'images': images}        
+    return render(request, 'photos/gallery.html', context)
 
 def view_photo(request, pk):
-    return render(request, 'photos/photo.html')  
+    image = Image.objects.get(id=pk) 
+    return render(request, 'photos/photo.html', {'image': image})  
 
 def add_photo(request):
     return render(request, 'photos/add.html')  
